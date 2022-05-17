6db8f8190b56b0987d2e896d67c9fd87a789a060
Char2s <Char2s@outlook.com>
2022-05-17 23:43:45 +0800

    feat: make port configurable

--- src/shared/env.constants.ts
+++ src/shared/env.constants.ts
@@ -4,11 +4,12 @@ import { env } from "process";
 config();
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
-const num = (k: string) => Number(env[k]);
+const num = (k: string) => env[k] && Number(env[k]);
 const str = (k: string) => env[k];
 const bool = (k: string) =>
   env[k] === "true" ? true : env[k] === "false" ? false : undefined;
 /* eslint-enable @typescript-eslint/no-unused-vars */
 
-export const DEBUG = bool("DEBUG");
-export const DB_URL = str("DB_URL");
+export const PORT = num("PORT") ?? 8080;
+export const DEBUG = bool("DEBUG") ?? false;
+export const DB_URL = str("DB_URL") ?? "data/db.sqlite3";
