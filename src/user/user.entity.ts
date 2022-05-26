fa686d172d0001e1ee894b1347eb15b4ab9ce392
Char2s <Char2s@outlook.com>
2022-05-26 10:56:27 +0800

    fix: adjust user password max length to allow hashed password

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -13,15 +13,20 @@ import { compare, hash } from "bcryptjs";
 import { DATABASE_PRE_INSERT } from "src/database/database-event";
 import { Entity } from "src/shared/entity";
 
+const HASH_LENGTH = 60;
+
 @entity.name("user")
 export class User extends Entity {
   override id: Entity["id"] = uuid(); // type info is lost during class inheritances (https://github.com/deepkit/deepkit-framework/issues/238), should be removed once fixed
   name!: string & MinLength<1> & MaxLength<20>;
   email!: Email & Unique;
-  password!: string & MinLength<6> & MaxLength<50> & Group<"hidden">;
+  password!: string &
+    MinLength<6> &
+    MaxLength<typeof HASH_LENGTH> &
+    Group<"hidden">;
 
   async hashPassword(): Promise<void> {
-    const hashed = this.password.length === 60;
+    const hashed = this.password.length === HASH_LENGTH;
     if (hashed) return;
     this.password = await hash(this.password, 10);
   }
