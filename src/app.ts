4e4f44701bb962cded45b9a6a45fc7847029d0e4
Char2s <Char2s@outlook.com>
2022-05-27 18:05:56 +0800

    refactor: define app entities in a separate file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import { App } from "@deepkit/app";
-import { ClassType } from "@deepkit/core";
 import { FrameworkModule } from "@deepkit/framework";
 
 import { AuthModule } from "./auth/auth.module";
+import { entities } from "./core/entities";
 import { RequestContext } from "./core/request-context";
 import { DatabaseModule } from "./database/database.module";
-import { User } from "./user/user.entity";
 import { UserModule } from "./user/user.module";
-
-const entities: ClassType[] = [User];
 
 new App({
   imports: [
---
+++ src/core/entities.ts
@@ -0,0 +1,4 @@
+import { ClassType } from "@deepkit/core";
+import { User } from "src/user/user.entity";
+
+export const entities: ClassType[] = [User];
