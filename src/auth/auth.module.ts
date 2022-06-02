d9c40727aaffda3666a5e736f82a97b3bf99cafb
Char2s <Char2s@outlook.com>
2022-06-02 19:30:47 +0800

    fix: add missing provider for auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,14 @@ import { createModule } from "@deepkit/app";
 import { JwtModule } from "src/jwt/jwt.module";
 
 import { AuthController } from "./auth.controller";
+import { AuthGuard } from "./auth.guard";
 import { AuthListener } from "./auth.listener";
 import { AuthTokenService } from "./auth-token.service";
 
 export class AuthModule extends createModule(
   {
     controllers: [AuthController],
-    providers: [AuthTokenService],
+    providers: [AuthTokenService, AuthGuard],
     listeners: [AuthListener],
   },
   "auth",
