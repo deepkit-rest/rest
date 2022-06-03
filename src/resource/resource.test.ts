cd608d42b760541ce5da3080b33818a8cfeafcb0
Char2s <Char2s@outlook.com>
2022-06-04 02:59:37 +0800

    test: complete resource crud tests

--- src/resource/resource.test.ts
+++ src/resource/resource.test.ts
@@ -5,12 +5,20 @@ import { http, HttpKernel, HttpQueries, HttpRequest } from "@deepkit/http";
 import { Inject, InjectorContext } from "@deepkit/injector";
 import { Logger, MemoryLoggerTransport } from "@deepkit/logger";
 import * as orm from "@deepkit/orm"; // temporary workaround: we have to use namespace import here as a temporary workaround, otherwise the application will not be able to bootstrap. This will be fixed in the next release
-import { AutoIncrement, entity, PrimaryKey } from "@deepkit/type";
+import {
+  AutoIncrement,
+  BackReference,
+  entity,
+  PrimaryKey,
+  Reference,
+} from "@deepkit/type";
 
 import { ResourceModule } from "./resource.module";
 import { ResourceCrudAdapter } from "./resource-crud-adapter.interface";
 import { ResourceCrudHandler } from "./resource-crud-handler.service";
+import { ResourceFilterMap } from "./resource-filter.typings";
 import { ResourcePagination } from "./resource-listing.typings";
+import { ResourceOrderMap } from "./resource-order.typings";
 
 describe("Resource", () => {
   describe("CRUD", () => {
@@ -22,6 +30,7 @@ describe("Resource", () => {
       entity: ClassType<Entity>,
       adapter: ClassType<ResourceCrudAdapter<Entity>>,
       controller: ClassType,
+      entities: ClassType[] = [],
     ) {
       facade = createTestingApp({
         imports: [new ResourceModule<Entity>().withAdapter(adapter)],
@@ -31,7 +40,7 @@ describe("Resource", () => {
             provide: "database",
             useValue: new orm.Database(
               new orm.MemoryDatabaseAdapter(), //
-              [entity],
+              [entity, ...entities],
             ),
           },
         ],
@@ -112,6 +121,98 @@ describe("Resource", () => {
           expect(response.statusCode).toBe(400);
         },
       );
+    });
+
+    describe("Filter", () => {
+      @entity.name("user")
+      class User {
+        id: number & AutoIncrement & PrimaryKey = 0;
+        books: Book[] & BackReference = [];
+      }
+      @entity.name("book")
+      class Book {
+        id: number & AutoIncrement & PrimaryKey = 0;
+        owner!: User & Reference;
+      }
+
+      class UserAdapter implements ResourceCrudAdapter<User> {
+        constructor(private db: Inject<orm.Database, "database">) {}
+        query(): orm.Query<User> {
+          return this.db.query(User);
+        }
+      }
+
+      @http.controller()
+      class UserController {
+        constructor(private handler: ResourceCrudHandler<User>) {}
+        @http.GET()
+        handle(filter: HttpQueries<ResourceFilterMap<User>>) {
+          return this.handler.list({ filter });
+        }
+      }
+
+      beforeEach(async () => {
+        await prepare(User, UserAdapter, UserController, [Book]);
+      });
+
+      it.each`
+        filter         | total | items
+        ${{}}          | ${3}  | ${[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        ${"id[$eq]=1"} | ${1}  | ${[{ id: 1 }]}
+        ${"id[$gt]=1"} | ${2}  | ${[{ id: 2 }, { id: 3 }]}
+      `("should work with query $filter", async ({ filter, total, items }) => {
+        await database.persist(new User(), new User(), new User());
+        const request = HttpRequest.GET("/");
+        request["queryPath"] = filter;
+        const response = await requester.request(request);
+        expect(response.json).toEqual({ total, items });
+      });
+    });
+
+    describe("Order", () => {
+      @entity.name("user")
+      class User {
+        id: number & AutoIncrement & PrimaryKey = 0;
+        books: Book[] & BackReference = [];
+      }
+      @entity.name("book")
+      class Book {
+        id: number & AutoIncrement & PrimaryKey = 0;
+        owner!: User & Reference;
+      }
+
+      class UserAdapter implements ResourceCrudAdapter<User> {
+        constructor(private db: Inject<orm.Database, "database">) {}
+        query(): orm.Query<User> {
+          return this.db.query(User);
+        }
+      }
+
+      @http.controller()
+      class UserController {
+        constructor(private handler: ResourceCrudHandler<User>) {}
+        @http.GET()
+        handle(order: HttpQueries<ResourceOrderMap<User>>) {
+          return this.handler.list({ order });
+        }
+      }
+
+      beforeEach(async () => {
+        await prepare(User, UserAdapter, UserController, [Book]);
+      });
+
+      it.each`
+        order        | items
+        ${{}}        | ${[{ id: 1 }, { id: 2 }]}
+        ${"id=asc"}  | ${[{ id: 1 }, { id: 2 }]}
+        ${"id=desc"} | ${[{ id: 2 }, { id: 1 }]}
+      `("should work with query $order", async ({ order, items }) => {
+        await database.persist(new User(), new User());
+        const request = HttpRequest.GET("/");
+        request["queryPath"] = order;
+        const response = await requester.request(request);
+        expect(response.json).toEqual({ total: 2, items });
+      });
     });
   });
 });
