ff1a8ed3c3c916c5f2337257f6d0fa74be4ed580
Char2s <Char2s@outlook.com>
2022-06-03 16:53:10 +0800

    test: complete tests for file module
    tests cannot pass currently because of a bug of deepkit

--- src/file/file.test.ts
+++ src/file/file.test.ts
@@ -8,6 +8,7 @@ import { entities } from "src/core/entities";
 import { RequestContext } from "src/core/request-context";
 import { DatabaseModule } from "src/database/database.module";
 import { DATABASE } from "src/database/database.tokens";
+import { FileEngine } from "src/file-engine/file-engine.interface";
 import { FileEngineModule } from "src/file-engine/file-engine.module";
 import { User } from "src/user/user.entity";
 
@@ -67,6 +68,7 @@ describe("File", () => {
         name: "test.txt",
         path: "/dir",
         size: 100,
+        contentRef: null,
         createdAt: expect.any(String),
       });
     });
@@ -92,10 +94,104 @@ describe("File", () => {
             name: "test.txt",
             path: "/dir",
             size: 100,
+            contentRef: null,
             createdAt: expect.any(String),
           },
         ],
       });
+    });
+  });
+
+  describe("GET /files/:id", () => {
+    it("should work", async () => {
+      const record = new FileRecord({
+        owner: user,
+        name: "test.txt",
+        path: "/dir",
+        size: 100,
+      });
+      await database.persist(record);
+      const response = await requester.request(
+        HttpRequest.GET(`/files/${record.id}`),
+      );
+      expect(response.json).toEqual({
+        id: record.id,
+        owner: user.id,
+        name: record.name,
+        path: record.path,
+        size: record.size,
+        contentRef: null,
+        createdAt: record.createdAt.toISOString(),
+      });
+    });
+  });
+
+  describe("PATCH /files/:id", () => {
+    it("should work", async () => {
+      const record = new FileRecord({
+        owner: user,
+        name: "test.txt",
+        path: "/dir",
+        size: 100,
+      });
+      await database.persist(record);
+      const response = await requester.request(
+        HttpRequest.PATCH(`/files/${record.id}`).json({ name: "updated" }),
+      );
+      expect(response.json).toEqual({
+        id: record.id,
+        owner: user.id,
+        name: "updated",
+        path: record.path,
+        size: record.size,
+        contentRef: null,
+        createdAt: record.createdAt.toISOString(),
+      });
+      const recordNew = await database.query(FileRecord).findOne();
+      expect(recordNew).toMatchObject({ name: "updated" });
+    });
+  });
+
+  describe("DELETE /files/:id", () => {
+    it("should work", async () => {
+      const record = new FileRecord({
+        owner: user,
+        name: "test.txt",
+        path: "/dir",
+        size: 100,
+      });
+      await database.persist(record);
+      const response = await requester.request(
+        HttpRequest.DELETE(`/files/${record.id}`),
+      );
+      expect(response.statusCode).toBe(204);
+      expect(response.bodyString).toBe("");
+      await expect(database.query(FileRecord).count()).resolves.toBe(0);
+    });
+  });
+
+  describe("PUT /files/:id/content", () => {
+    it("should work", async () => {
+      const record = new FileRecord({
+        owner: user,
+        name: "test.txt",
+        path: "/dir",
+        size: 100,
+      });
+      await database.persist(record);
+      const fileEngine = facade.app.get(FileEngine);
+      const fileEngineStoreSpy = jest
+        .spyOn(fileEngine, "store")
+        .mockReturnValue(Promise.resolve("ref"));
+      const response = await requester.request(
+        HttpRequest.PUT(`/files/${record.id}/content`).body(Buffer.from("v")),
+      );
+      expect(response.statusCode).toBe(204);
+      expect(response.bodyString).toBe("");
+      const recordNew = await database.query(FileRecord).findOne();
+      expect(recordNew.contentRef).toBeDefined();
+      expect(fileEngineStoreSpy).toHaveBeenCalledTimes(1);
+      expect(fileEngineStoreSpy.mock.calls[0][0].read().toString()).toBe("v");
     });
   });
 });
