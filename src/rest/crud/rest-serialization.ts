95b0ace3142fcd4641a071e177a317ec5bd303b0
Char2s <Char2s@outlook.com>
2022-07-16 15:26:27 +0800

    feat(rest): make generic serializer more extensive

--- src/rest/crud/rest-serialization.ts
+++ src/rest/crud/rest-serialization.ts
@@ -25,16 +25,9 @@ export class RestGenericSerializer<Entity>
 
   async create(payload: Record<string, unknown>): Promise<Entity> {
     const entityType = this.context.getEntitySchema().getClassType();
-    if (entityType.length) {
-      const message =
-        "Entity constructor should take no params for generic creation";
-      throw new Error(message);
-    }
     const schema = this.creationSchemaFactory.build(entityType);
-    const data = purify(payload, schema.type);
-    const entity: Entity = new entityType();
-    Object.assign(entity, data);
-    return entity;
+    const data = purify<Partial<Entity>>(payload, schema.type);
+    return this.createEntity(data);
   }
 
   async update(
@@ -43,7 +36,23 @@ export class RestGenericSerializer<Entity>
   ): Promise<Entity> {
     const entityType = this.context.getEntitySchema().getClassType();
     const schema = this.updateSchemaFactory.build(entityType);
-    const data = purify(payload, schema.type);
+    const data = purify<Partial<Entity>>(payload, schema.type);
+    return this.updateEntity(entity, data);
+  }
+
+  protected createEntity(data: Partial<Entity>): Entity {
+    const entityType = this.context.getEntitySchema().getClassType();
+    if (entityType.length) {
+      const message =
+        "Entity constructor should take no params for generic creation";
+      throw new Error(message);
+    }
+    const entity: Entity = new entityType();
+    Object.assign(entity, data);
+    return entity;
+  }
+
+  protected updateEntity(entity: Entity, data: Partial<Entity>): Entity {
     Object.assign(entity, data);
     return entity;
   }
