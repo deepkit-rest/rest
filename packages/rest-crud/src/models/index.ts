8d1ee76796a71c2a4782139848739db345f563d4
Char2s <Char2s@outlook.com>
2022-08-24 20:07:29 +0800

    refactor(rest-crud): adjust export statements

--- packages/rest-crud/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./rest-creation-schema";
-export * from "./rest-entity-model";
-export * from "./rest-filter-map";
-export * from "./rest-order-map";
-export * from "./rest-update-schema";
